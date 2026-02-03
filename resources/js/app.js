07ab351556471fc81d79517cd1273598365b3e8a
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-02-03 22:17:20 +0530

    fix: add Alpine theme import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
-import './bootstrap';
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
+
+import theme from "./theme";
 
 window.Alpine = Alpine;
+
+Alpine.store("theme", theme);
 
 Alpine.start();
