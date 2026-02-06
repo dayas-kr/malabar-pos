fad4268af2ad0e259e2b4b12816fd541d6be9b42
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2026-02-06 18:58:06 +0530

    fix(ui): Add Alpine plugins anchor and focus

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 import "./bootstrap";
 
 import Alpine from "alpinejs";
-
+import anchor from "@alpinejs/anchor";
+import focus from "@alpinejs/focus";
 import theme from "./theme";
 
+Alpine.plugin(anchor);
+Alpine.plugin(focus);
 window.Alpine = Alpine;
 
 Alpine.store("theme", theme);
